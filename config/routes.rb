3a508eb9800674460d2370212a638b1bf5c8e2f5
fksgshota <fksg.shotaness@gmail.com>
2021-06-04 01:22:49 +0900

    fix typo

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,9 @@ Rails.application.routes.draw do
 
   resources :retirements, only: %i[new create]
 
-  resources :events only: %i[new create show edit update destroy] do
+  resources :events, only: %i[new create show edit update destroy] do
     resources :tickets, only: %i[new create destroy]
   end
 
-  match "*path" => "application#error404", via: :all
+  match '*path' => 'application#error404', via: :all
 end
